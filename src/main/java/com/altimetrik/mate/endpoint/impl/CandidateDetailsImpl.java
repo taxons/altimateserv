8ec484e33c29ca70055b9c2ca8b0ddb238c18add
PERUMAL M, LEKSHMANA <lperumalm@paypal.com>
2018-01-06 04:49:01 +0530

    Changed List code

--- src/main/java/com/altimetrik/mate/endpoint/impl/CandidateDetailsImpl.java
+++ src/main/java/com/altimetrik/mate/endpoint/impl/CandidateDetailsImpl.java
@@ -21,104 +21,96 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Component
 @Scope("request")
 public class CandidateDetailsImpl implements CandidateDetails {
-    
-    public static final String ALTI_API_PREFIX = "/v1/manage/candidate/get?id=";
 
-    private static List<Candidate> listCandidate=new ArrayList<>();
-    
-    static {
-        Candidate candidate1= new Candidate();
-        candidate1.setId(System.currentTimeMillis());
-        candidate1.setName("Naveen");
-        candidate1.setEmail("naveen@gmail.com");
-        candidate1.setAge(25);
-        candidate1.setPhone("9991828929");
-        candidate1.setExperience(new Experience(5,3));
-        listCandidate.add(candidate1);
-        
-        Candidate candidate2= new Candidate();
-        candidate2.setId(System.currentTimeMillis());
-        candidate2.setName("Lakeshman");
-        candidate2.setEmail("lak@gmail.com");
-        candidate2.setAge(25);
-        candidate2.setPhone("9991828929");
-        candidate2.setExperience(new Experience(5,3));
-        listCandidate.add(candidate2);
-        
-        Candidate candidate3= new Candidate();
-        candidate3.setId(System.currentTimeMillis());
-        candidate3.setName("arun");
-        candidate3.setEmail("arun@gmail.com");
-        candidate3.setAge(25);
-        candidate3.setPhone("9991828929");
-        candidate3.setExperience(new Experience(5,3));
-        listCandidate.add(candidate3);
-        
-        Candidate candidate4= new Candidate();
-        candidate4.setId(System.currentTimeMillis());
-        candidate4.setName("joshi");
-        candidate4.setEmail("joshi@gmail.com");
-        candidate4.setAge(25);
-        candidate4.setPhone("9991828929");
-        candidate4.setExperience(new Experience(5,3));
-        listCandidate.add(candidate4);
-        
-        Candidate candidate5= new Candidate();
-        candidate5.setId(System.currentTimeMillis());
-        candidate5.setName("aarthi");
-        candidate5.setEmail("aarthi@gmail.com");
-        candidate5.setAge(25);
-        candidate5.setPhone("9991828929");
-        candidate5.setExperience(new Experience(5,3));
-        listCandidate.add(candidate5);
-        
-    }   
+	public static final String ALTI_API_PREFIX = "/v1/manage/candidate/get?id=";
 
-    @Override
-    public Response add(Candidate c) {
-        c.setId(System.currentTimeMillis());
-        listCandidate.add(c);
-        Link link = new Link();
-        link.setHref(ALTI_API_PREFIX+c.getId());
-        Response build = Response.ok(link).build();
-        return build;
-    }
+	private static List<Candidate> listCandidate = new ArrayList<>();
 
-    @Override
-    public Response search(String email, String phone, String id) throws JsonProcessingException {
-        for(Candidate candidate:listCandidate) {
-            if(email.equals(candidate.getEmail())) {
-                ObjectMapper mapper = new ObjectMapper();
-                String jsonInString = mapper.writeValueAsString(candidate);
-                return Response.ok(jsonInString).build();
-            }
-        }
-        return Response.status(404).build();
-    }
+	static {
+		Candidate candidate1 = new Candidate();
+		candidate1.setId(System.currentTimeMillis());
+		candidate1.setName("Naveen");
+		candidate1.setEmail("naveen@gmail.com");
+		candidate1.setAge(25);
+		candidate1.setPhone("9991828929");
+		candidate1.setExperience(new Experience(5, 3));
+		listCandidate.add(candidate1);
 
-    @Override
-    public Response get(String id) throws JsonProcessingException {
-        for(Candidate candidate:listCandidate) {
-            if(String.valueOf(candidate.getId()).equals(id)) {
-                ObjectMapper mapper = new ObjectMapper();
-                String jsonInString = mapper.writeValueAsString(candidate);
-                return Response.ok(jsonInString).build();
-            }
-        }
-        return Response.status(404).build();
-    }
+		Candidate candidate2 = new Candidate();
+		candidate2.setId(System.currentTimeMillis());
+		candidate2.setName("Lakeshman");
+		candidate2.setEmail("lak@gmail.com");
+		candidate2.setAge(25);
+		candidate2.setPhone("9991828929");
+		candidate2.setExperience(new Experience(5, 3));
+		listCandidate.add(candidate2);
 
-    @Override
-    public Response getAll() throws JsonProcessingException {
-        List<String> getAllCandidate=new ArrayList<>();
-        for(Candidate candidate:listCandidate) {
-            ObjectMapper mapper = new ObjectMapper();
-            String jsonInString = mapper.writeValueAsString(candidate);
-            getAllCandidate.add(jsonInString);
-        }
-        return Response.ok(getAllCandidate).build();
-    }
+		Candidate candidate3 = new Candidate();
+		candidate3.setId(System.currentTimeMillis());
+		candidate3.setName("arun");
+		candidate3.setEmail("arun@gmail.com");
+		candidate3.setAge(25);
+		candidate3.setPhone("9991828929");
+		candidate3.setExperience(new Experience(5, 3));
+		listCandidate.add(candidate3);
 
-   
+		Candidate candidate4 = new Candidate();
+		candidate4.setId(System.currentTimeMillis());
+		candidate4.setName("joshi");
+		candidate4.setEmail("joshi@gmail.com");
+		candidate4.setAge(25);
+		candidate4.setPhone("9991828929");
+		candidate4.setExperience(new Experience(5, 3));
+		listCandidate.add(candidate4);
+
+		Candidate candidate5 = new Candidate();
+		candidate5.setId(System.currentTimeMillis());
+		candidate5.setName("aarthi");
+		candidate5.setEmail("aarthi@gmail.com");
+		candidate5.setAge(25);
+		candidate5.setPhone("9991828929");
+		candidate5.setExperience(new Experience(5, 3));
+		listCandidate.add(candidate5);
+
+	}
+
+	@Override
+	public Response add(Candidate c) {
+		c.setId(System.currentTimeMillis());
+		listCandidate.add(c);
+		Link link = new Link();
+		link.setHref(ALTI_API_PREFIX + c.getId());
+		Response build = Response.ok(link).build();
+		return build;
+	}
+
+	@Override
+	public Response search(String email, String phone, String id) throws JsonProcessingException {
+		for (Candidate candidate : listCandidate) {
+			if (email.equals(candidate.getEmail())) {
+				return Response.ok(candidate).build();
+			}
+		}
+		return Response.status(404).build();
+	}
+
+	@Override
+	public Response get(String id) throws JsonProcessingException {
+		for (Candidate candidate : listCandidate) {
+			if (String.valueOf(candidate.getId()).equals(id)) {
+				return Response.ok(candidate).build();
+			}
+		}
+		return Response.status(404).build();
+	}
+
+	@Override
+	public Response getAll() throws JsonProcessingException {
+		List<Candidate> getAllCandidate = new ArrayList<>();
+		for (Candidate candidate : listCandidate) {
+			getAllCandidate.add(candidate);
+		}
+		return Response.ok(getAllCandidate).build();
+	}
 
 }
