5c6f63928441e559f0aa3bf33ad5b277e7a0cf6c
joshi chandran <jochandran@paypal.com>
2018-01-06 03:57:30 +0530

    get candidate changes

--- src/main/java/com/altimetrik/mate/endpoint/impl/CandidateDetailsImpl.java
+++ src/main/java/com/altimetrik/mate/endpoint/impl/CandidateDetailsImpl.java
@@ -5,21 +5,25 @@ import java.util.List;
 
 import javax.ws.rs.core.Response;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.altimetrik.mate.endpoint.CandidateDetails;
 import com.altimetrik.mate.model.Candidate;
 import com.altimetrik.mate.model.Link;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * TODO: Describe purpose and behavior of CandidateDetailsImpl
  */
 @Component
+@Scope("request")
 public class CandidateDetailsImpl implements CandidateDetails {
     
-    public static final String ALTI_API_PREFIX = "/v1/manage/";
+    public static final String ALTI_API_PREFIX = "/v1/manage/candidate/get?id=";
 
-    private List<Candidate> listCandidate=new ArrayList<>();
+    private static List<Candidate> listCandidate=new ArrayList<>();
 
     @Override
     public Response add(Candidate c) {
@@ -27,17 +31,26 @@ public class CandidateDetailsImpl implements CandidateDetails {
         listCandidate.add(c);
         Link link = new Link();
         link.setHref(ALTI_API_PREFIX+c.getId());
-        return Response.ok(link).build();
+        Response build = Response.ok(link).build();
+        return build;
     }
 
     @Override
     public Response search(String email, String phone, String id) {
+        
         return null;
     }
 
     @Override
-    public Response get(String id) {
-        return null;
+    public Response get(String id) throws JsonProcessingException {
+        for(Candidate candidate:listCandidate) {
+            if(String.valueOf(candidate.getId()).equals(id)) {
+                ObjectMapper mapper = new ObjectMapper();
+                String jsonInString = mapper.writeValueAsString(candidate);
+                return Response.ok(jsonInString).build();
+            }
+        }
+        return Response.status(404).build();
     }
 
 }
